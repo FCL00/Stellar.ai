57f5604e374e7b40bbce1f8b38432ce671bacf0a
FCL00 <flagahit00@gmail.com>
2025-05-07 23:02:46 +0800

    chore: add new wish list form for hero section

---
+++ src/components/ui/WaitlistForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative max-w-lg space-y-2 md:space-y-0">
+        <input type="text" class="w-full p-4 text-white border rounded-lg outline-none bg-black-0 border-black-30 focus:outline-violet-500" placeholder="your@email.com">
+        <button class="relative w-full p-4 bg-white rounded-md md:w-fit md:p-2 md:absolute text-black-0 md:right-2 md:top-2">
+            <span>Join Waitlist</span>
+        </button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style scoped>
+   
+</style>
